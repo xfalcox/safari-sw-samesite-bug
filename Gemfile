23d3757ec5169aa8a798fd68865a722d6a477fb6
Rafael dos Santos Silva <xfalcox@gmail.com>
2019-07-30 21:19:17 -0300

    remove deps

--- Gemfile
+++ Gemfile
@@ -1,5 +1,3 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
-gem 'sinatra-contrib'
-gem 'secure_headers'
--- app.rb
+++ app.rb
@@ -1,29 +1,11 @@
 require 'sinatra'
-require 'sinatra/cookies'
-require 'secure_headers'
-
-use SecureHeaders::Middleware
-
-SecureHeaders::Configuration.default do |config|
-  config.cookies = {
-    secure: true,
-    httponly: true,
-    samesite: {
-      lax: true # mark all cookies as SameSite=Strict
-    }
-  }
-  config.csp = {
-    default_src: %w('unsafe-inline' 'self'),
-    script_src: %w('unsafe-inline' 'self' storage.googleapis.com)
-  }  
-end
 
 get '/' do
-  @cookie = cookies[:time]
+  @cookie = request.cookies['time']
   erb :index
 end
 
-get '/set-cookies' do 
-  cookies[:time] = Time.now
+get '/set-cookies' do
+  response.set_cookie(:time, value: Time.now, expires: Time.now + 3600 * 24, httponly: true, same_site: 'Lax')
   redirect to('/')
 end
